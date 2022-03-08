bcdd62324555f98217cf9d3105170b4bd95e6f4b
Fábio Rocha <86632788+FabioRocha462@users.noreply.github.com>
2022-03-07 21:17:51 -0300

    fiz comentários

--- atividade Assembly/desafio7.asm
+++ atividade Assembly/desafio7.asm
@@ -28,10 +28,10 @@ esp: .asciiz " "
    #ordenando o vetor
    li $t1,0
    li $t2,12
-   loopExterno:
+   loopExterno:#loop externo, para pegar o valor de comparação
      beq $t1,$t2, saiDoLoopExterno#regra para sair do loop 
      addi $t3,$t1,4
-     loopInterno:
+     loopInterno:#loop interno para fazer a comparação entre dois valores e fazer a troca, caso seja necessário
      	beq $t3,$t2, saiDoLoopInterno#regra para sair do loop 
      	lw $t4, myArray($t1)
      	lw $t5, myArray($t3)
@@ -45,7 +45,7 @@ esp: .asciiz " "
    
    saiDoLoopExterno:
     li $t1,0
-    loopImprimi:
+    loopImprimi:#loop para imprimir
       beq $t1,$t2, saiDoLoopImprimi
       lw $t3,myArray($t1)
       
@@ -63,14 +63,14 @@ esp: .asciiz " "
   li $v0,10
   syscall
    #minhas funções
-   com:
-   move $t6,$t5
-   move $t5,$t4
-   move $t4,$t6
-   sw $t4, myArray($t1)
-   sw $t5, myArray($t3)
-   jal pivo
+   com:;#função para fazer o swap dos valores, caso a condição de troca seja verdadeira
+   move $t6,$t5#passo o valor de $t5 para um registrador auxiliar
+   move $t5,$t4#passo o valor de $t4 para $t5
+   move $t4,$t6#passo para $t4 o valor recuperado
+   sw $t4, myArray($t1) #passo o valor do registrador para  a memória
+   sw $t5, myArray($t3) #passo o valor do registrador para a memória
+   jal pivo #pulo para o pivo
    
    pivo:
    addi $t3,$t3,4 #função que incrementa caso o número novo seja maior que o atual maior
-   jal loopInterno
+   jal loopInterno #pulo para loopInterno, escapando de uma parada inesperada
